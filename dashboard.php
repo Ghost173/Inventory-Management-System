51074d5a867abdde427a9d61905f52699cfceaab
ghost <jpirakas007@gmail.com>
2019-03-27 14:51:42 +0530

    INV dashboard

---
+++ dashboard.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Inventory-Management-System</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script type="text/javascript" src="./js/main.js"></script>
+</head>
+<body>
+    
+<!-- <div class="jumbotron"><h1>hello world</h1></div> -->
+<?php
+include_once("./templates/header.php");
+?>
+<!-- card -->
+<p><br/></p>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+            <img src="./images/user.png" class="card-img-top" alt="user">
+            <div class="card-body">
+                <h5 class="card-title">Profile Info</h5>
+                <p class="card-text">ROOT GHOST</p>
+                <p class="card-text">Admin</p>
+                <p class="card-text">Last Login : xxxx-xx-xx</p>
+                <a href="#" class="btn btn-primary">Edit profile</a>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
