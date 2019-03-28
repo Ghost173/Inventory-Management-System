2de62ee102a97bb7e194e1cac4dde222f3a7771b
ghost <jpirakas007@gmail.com>
2019-03-28 10:59:51 +0530

    register design

---
+++ register.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Inventory-Management-System</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script type="text/javascript" src="./js/main.js"></script>
+</head>
+<body>
+    
+<!-- <div class="jumbotron"><h1>hello world</h1></div> -->
+<?php
+include_once("./templates/header.php");
+?>
+<div class="container">
+		<div class="card mx-auto" style="width: 30rem;">
+	        <div class="card-header">Register</div>
+		      <div class="card-body">
+		        <form id="register_form" onsubmit="return false" autocomplete="off">
+		          <div class="form-group">
+		            <label for="username">Full Name</label>
+		            <input type="text" name="username" class="form-control" id="username" placeholder="Enter Username">
+		            <small id="u_error" class="form-text text-muted"></small>
+		          </div>
+		          <div class="form-group">
+		            <label for="email">Email address</label>
+		            <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+		            <small id="e_error" class="form-text text-muted">We'll never share your email with anyone else.</small>
+		          </div>
+		          <div class="form-group">
+		            <label for="password1">Password</label>
+		            <input type="password" name="password1" class="form-control"  id="password1" placeholder="Password">
+		            <small id="p1_error" class="form-text text-muted"></small>
+		          </div>
+		          <div class="form-group">
+		            <label for="password2">Re-enter Password</label>
+		            <input type="password" name="password2" class="form-control"  id="password2" placeholder="Password">
+		            <small id="p2_error" class="form-text text-muted"></small>
+		          </div>
+		          <div class="form-group">
+		            <label for="usertype">Usertype</label>
+		            <select name="usertype" class="form-control" id="usertype">
+		              <option value="">Choose User Type</option>
+		              <option value="Admin">Admin</option>
+		              <option value="Other">Other</option>
+		            </select>
+		            <small id="t_error" class="form-text text-muted"></small>
+		          </div>
+		          <button type="submit" name="user_register" class="btn btn-primary"><span class="fa fa-user"></span>&nbsp;Register</button>
+		          <span><a href="index.php">Login</a></span>
+		        </form>
+		      </div>
+	      <div class="card-footer text-muted">
+	        
+	      </div>
+	    </div>
+	</div>
+ 
+
+</body>
+</html>
